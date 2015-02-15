6f58576f2379ce05e09267061e8cbaf50ae8c7bc
ssuryanv <ssuryan.v@gmail.com>
2015-02-14 22:03:58 -0500

    Version 13
    Major Changes, including JavaScript

---
+++ excel.js
@@ -0,0 +1,9 @@
+function excel(){
+   var a = prompt("Type in the password.")
+   if(a == "r40slant"){
+	   window.open("excel.html")
+   }
+   else{
+	   alert("WRONG!/nThis is not the right password!")
+   }
+}
